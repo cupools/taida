e64302080914dcc080d16916b51a477c0095b7d9
liyh <lyh106415@alibaba-inc.com>
2016-09-23 20:59:57 +0800

    update test

--- test/tinifier.spec.js
+++ test/tinifier.spec.js
@@ -11,20 +11,39 @@ describe('tinifier', function () {
   describe('should work', function () {
     let buffer = fs.readFileSync('test/fixtures/0.png')
 
-    it('should not throw exception', function (done) {
+    it('get correct information', function (done) {
       tinifier(buffer)
         .then(data => {
           expect(data).to.have.property('buffer')
+            .that.to.be.an.instanceof(Buffer)
           expect(data).to.have.property('size')
+            .that.is.a('number')
+          expect(data).to.have.deep.property('origin.buffer')
+            .that.to.be.an.instanceof(Buffer)
+          expect(data).to.have.deep.property('origin.size')
             .that.is.a('number')
           expect(data).to.have.property('level')
             .that.is.a('number')
             .that.to.be.within(0, 1)
-
           done()
         })
         .catch(err => {
-          console.log(err)
+          done(err)
+        })
+    })
+  })
+
+  describe('should catch error with unexpected bitmap', function () {
+    let buffer = fs.readFileSync('test/fixtures/bad.png')
+
+    it('get correct callback', function (done) {
+      tinifier(buffer)
+        .then(() => {
+          done(new Error('should not resolve promise'))
+        })
+        .catch(err => {
+          expect(err).to.have.property('message')
+            .that.to.contain('HTTP 415/Unsupported file type')
           done()
         })
     })
