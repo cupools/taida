4836cb1f6382dee1557a4aa8b4261d6f19960064
liyh <lyh106415@alibaba-inc.com>
2016-09-29 14:25:02 +0800

    update apikey

--- src/apikey.js
+++ src/apikey.js
@@ -39,15 +39,68 @@ function write(keypath, apikeys = []) {
 export default {
   __path: path.resolve('.apikey'),
   __apikeys: null,
+
   get apikeys() {
-    return this.__apikeys ? this.__apikeys : (this.__apikeys = read(this.__path))
+    if (this.__apikeys) {
+      return this.apikeys
+    }
+
+    // get apikeys and revise invalid keys
+    let now = Date.now()
+    let json = read(this.__path)
+    let needUpdate = false
+    let revise = json.map(item => {
+      if (!item.valid && now - item.date > 1 * 24 * 3600 * 1e3) {
+        needUpdate = true
+        return Object.assign({}, item, {
+          valid: true,
+          date: now
+        })
+      }
+      return item
+    })
+
+    // update .apikey
+    if (needUpdate) {
+      write(this.__path, revise)
+    }
+
+    // save keys to instance
+    if (revise) {
+      this.__apikeys = revise
+    }
+
+    return this.__apikeys
   },
+
   set apikeys(val) {
     return this.__apikeys
   },
+
+  get() {
+    let alternate = this.apikeys ? this.apikeys.filter(item => item.valid) : []
+    return alternate.length ? alternate[0].key : null
+  },
+
+  depress() {
+    let target = this.get()
+
+    if (target) {
+      let index = this.__apikeys.map(item => item.key).indexOf(target)
+
+      this.__apikeys[index].valid = false
+      this.__apikeys[index].date = Date.now()
+
+      write(this.__path, this.__apikeys)
+    } else {
+      log.warn('No valid apikey currently')
+    }
+  },
+
   add(...keys) {
     let {apikeys} = this
 
+    // TODO, arguments' validation
     let args = keys.reduce(
       (ret, key) => {
         if (apikeys.includes(key)) {
@@ -62,10 +115,10 @@ export default {
     if (args.length) {
       log.info(`${args.length} apikeys has been added successful and ${keys.length - args.length} failed.`)
     }
+
+    return this
   },
-  get() {
-    return (this.apikeys && this.apikeys[0]) || null
-  },
+
   delete(key) {
     let apikeys = this.apikeys
     let index = isNaN(key) ? Number(key) : apikeys.indexOf(key)
@@ -76,17 +129,24 @@ export default {
     } else {
       log.warn(`The key \`${key}\` hasn't been added before.`)
     }
+
+    return this
   },
+
   clear() {
     this.apikeys.length = 0
+    return this
   },
-  list() {
 
+  list() {
+    return this
   },
+
   edit() {
     child.exec(`open ${this.__path}`)
   },
-  apply() {
+
+  supply() {
     child.exec('open https://tinypng.com/developers')
   }
 }
--- src/tinifier.js
+++ src/tinifier.js
@@ -2,31 +2,60 @@ import tinify from 'tinify'
 
 import apikey from './apikey'
 
-const tinifier = function (buffer) {
-  tinify.key = apikey.get()
+const tinifier = function () {
+  setKey()
 
+  return function (buffer) {
+    return compress(buffer).catch(error => handleError(compress, error))
+  }
+}
+
+function setKey() {
+  tinify.key = apikey.get()
+}
+
+function handleError(task, ret) {
+  let {error, buffer} = ret
+  let {message} = error
+
+  if (message.includes(401)) {
+    // Credentials are invalid (HTTP 401/Unauthorized)
+    // should change another apikey and fallback to compress
+
+    return task(buffer)
+  }
+
+  return Promise.reject(error)
+}
+
+function compress(buffer) {
   return new Promise(
     (resolve, reject) => tinify
       .fromBuffer(buffer)
       .toBuffer(
-        (error, data) => (
-          error ? reject(error) : resolve({
-            buffer: data,
-            size: data.length,
-            origin: {
-              buffer,
-              size: buffer.length
-            },
-            level: Number((data.length / buffer.length).toFixed(4))
-          })
-        )
+        (error, data) => {
+          if (error) {
+            reject({
+              error,
+              buffer
+            })
+          } else {
+            resolve({
+              buffer: data,
+              size: data.length,
+              origin: {
+                buffer,
+                size: buffer.length
+              },
+              level: Number((data.length / buffer.length).toFixed(4))
+            })
+          }
+        }
       )
-  ).catch(error => {
-    
-  })
+  )
 }
 
 tinifier.tinify = tinify
 tinifier.apikey = apikey
 
-export default tinifier
+export default tinifier()
